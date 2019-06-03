b490bd8f07a1718bc59787e37b09203c8c7ae8a8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-02 22:53:05 -0400

    removed client directory

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ShoppingList from "./components/ShoppingList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ShoppingList />
+      </div>
+    );
+  }
+}
+
+export default App;
